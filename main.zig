b8b32659f97959befb84cb61154159fbc50b9420
claw5ie <92851066+claw5ie@users.noreply.github.com>
2023-07-27 13:51:46 +0100

    Add springs and fix force clipping on collision
    Component of the force that is in direction of displacement between
    balls should only be canceled when it is directed towards the
    collision plane.

--- main.zig
+++ main.zig
@@ -17,6 +17,21 @@ const SCREEN_HEIGHT = 600.0;
 const UP = SCREEN_HEIGHT / SCREEN_WIDTH;
 const DOWN = -SCREEN_HEIGHT / SCREEN_WIDTH;
 
+const Spring = struct {
+    start: usize,
+    end: usize,
+
+    stiffness: f32,
+    rest_length: f32,
+    min_length: f32,
+    max_length: f32,
+    damping_factor: f32,
+
+    half_height: f32,
+    hand_length: f32,
+    blade_count: usize,
+};
+
 const Balls = struct {
     pos: [*]Vec2,
     vel: [*]Vec2,
@@ -25,6 +40,7 @@ const Balls = struct {
     mass: [*]f32,
     radius: [*]f32,
     color: [*]ray.Color,
+    edges: []Spring,
     count: usize,
 };
 
@@ -78,8 +94,8 @@ fn rand_range(min: f32, max: f32) f32 {
 
 fn normalize_v2(v: Vec2) Vec2 {
     var result = v;
-    var len = abs_v2(v);
-    result /= @splat(2, len);
+    var length = abs_v2(v);
+    result /= @splat(2, length);
 
     return result;
 }
@@ -156,78 +172,106 @@ fn add_gravity_force(balls: *Balls, i: usize, j: usize) void {
     const GRAVITY_CONSTANT = 0.005;
 
     var disp = balls.pos[j] - balls.pos[i];
-    var disp_len = abs_v2(disp);
-    var force = GRAVITY_CONSTANT * balls.mass[i] * balls.mass[j] / (disp_len * disp_len);
+    var disp_length = abs_v2(disp);
+    var force = GRAVITY_CONSTANT * balls.mass[i] * balls.mass[j] / (disp_length * disp_length);
 
-    disp /= @splat(2, disp_len);
+    disp /= @splat(2, disp_length);
 
     balls.force[i] += disp * @splat(2, force);
     balls.force[j] -= disp * @splat(2, force);
 }
 
-fn add_spring_force(balls: *Balls, i: usize, j: usize) void {
-    const STIFFNESS = 1.0;
-    const REST_LENGTH = 0.1;
-    const DAMPING_FACTOR = 0.1;
-
+fn add_spring_force(balls: *Balls, spring: Spring) void {
+    var i = spring.start;
+    var j = spring.end;
     var disp = balls.pos[j] - balls.pos[i];
-    var disp_len = abs_v2(disp);
+    var disp_length = abs_v2(disp);
 
-    disp /= @splat(2, disp_len);
+    disp /= @splat(2, disp_length);
 
-    var damping = dot(disp, balls.vel[j] - balls.vel[i]) * DAMPING_FACTOR;
-    var force = STIFFNESS * (disp_len - REST_LENGTH) + damping;
+    var damping = dot(disp, balls.vel[j] - balls.vel[i]) * spring.damping_factor;
+    var force = spring.stiffness * (disp_length - spring.rest_length) + damping;
 
     balls.force[i] += @splat(2, force) * disp;
     balls.force[j] -= @splat(2, force) * disp;
-}
 
-const SpringInfo = struct {
-    half_height: f32,
-    hand_length: f32,
-    blade_count: usize,
-};
+    var overlap = disp_length - spring.max_length;
+    if (overlap > 0) {
+        overlap /= 2;
+        balls.disp[i] += @splat(2, overlap) * disp;
+        balls.disp[j] -= @splat(2, overlap) * disp;
+
+        var force_disp_comp = dot(balls.force[i], disp);
+        if (force_disp_comp < 0) {
+            balls.force[i] -= @splat(2, force_disp_comp) * disp;
+        }
+
+        force_disp_comp = dot(balls.force[j], disp);
+        if (force_disp_comp > 0) {
+            balls.force[j] -= @splat(2, force_disp_comp) * disp;
+        }
+    }
+
+    overlap = disp_length - spring.min_length;
+    if (overlap < 0) {
+        overlap /= 2;
+        balls.disp[i] += @splat(2, overlap) * disp;
+        balls.disp[j] -= @splat(2, overlap) * disp;
+
+        var force_disp_comp = dot(balls.force[i], disp);
+        if (force_disp_comp > 0) {
+            balls.force[i] -= @splat(2, force_disp_comp) * disp;
+        }
+
+        force_disp_comp = dot(balls.force[j], disp);
+        if (force_disp_comp < 0) {
+            balls.force[j] -= @splat(2, force_disp_comp) * disp;
+        }
+    }
+}
 
 var spring_points_buffer: [32]Vec2 = undefined;
 
-fn draw_spring(info: SpringInfo, start: Vec2, end: Vec2, color: ray.Color) void {
-    var peak_count = 2 * info.blade_count - @boolToInt(info.blade_count % 2 == 0);
+fn draw_spring(balls: *Balls, spring: Spring, color: ray.Color) void {
+    var start = balls.pos[spring.start];
+    var disp = balls.pos[spring.end] - start;
+    var full_length = abs_v2(disp);
+
+    disp /= @splat(2, full_length);
+
+    var peak_count = 2 * spring.blade_count - @boolToInt(spring.blade_count % 2 == 0);
     var point_count = peak_count + 2 * 2;
     var points: []Vec2 = spring_points_buffer[0..point_count];
-    var full_len = abs_v2(end - start);
 
     points[0] = Vec2{ 0, 0 };
-    points[1] = Vec2{ info.hand_length, 0 };
+    points[1] = Vec2{ spring.hand_length, 0 };
 
-    points[point_count - 2] = Vec2{ full_len - info.hand_length, 0 };
-    points[point_count - 1] = Vec2{ full_len, 0 };
+    points[point_count - 2] = Vec2{ full_length - spring.hand_length, 0 };
+    points[point_count - 1] = Vec2{ full_length, 0 };
 
     {
-        var base_len = (full_len - 2 * info.hand_length) / @intToFloat(f32, peak_count);
+        var base_length = (full_length - 2 * spring.hand_length) / @intToFloat(f32, peak_count);
         var peak = Vec2{
-            points[1][0] + base_len / 2,
-            info.half_height,
+            points[1][0] + base_length / 2,
+            (std.math.clamp(full_length, spring.min_length, spring.max_length) - spring.max_length) / (spring.min_length - spring.max_length) * spring.half_height,
         };
 
         var i: usize = 0;
         while (i < peak_count) : (i += 1) {
             points[i + 2] = peak;
             peak[1] = -peak[1];
-            peak[0] += base_len;
+            peak[0] += base_length;
         }
     }
 
     {
-        var d = end - start;
-        d /= @splat(2, full_len);
-
         var i: usize = 0;
         while (i < point_count) : (i += 1) {
             var x = points[i][0];
             var y = points[i][1];
             points[i] = .{
-                start[0] + d[0] * x - d[1] * y,
-                start[1] + d[1] * x + d[0] * y,
+                start[0] + disp[0] * x - disp[1] * y,
+                start[1] + disp[1] * x + disp[0] * y,
             };
         }
     }
@@ -246,19 +290,11 @@ fn draw_balls(balls: *Balls) void {
     var i: usize = 0;
     while (i < balls.count) : (i += 1) {
         balls.force[i] = .{ 0, 0 };
-    }
-
-    i = 0;
-    while (i < balls.count) : (i += 1) {
-        var j: usize = i + 1;
-        while (j < balls.count) : (j += 1) {
-            add_spring_force(balls, i, j);
-        }
-    }
-
-    i = 0;
-    while (i < balls.count) : (i += 1) {
         balls.disp[i] = .{ 0, 0 };
+    }
+
+    for (balls.edges) |edge| {
+        add_spring_force(balls, edge);
     }
 
     i = 0;
@@ -268,35 +304,33 @@ fn draw_balls(balls: *Balls) void {
         var j: usize = i + 1;
         while (j < balls.count) : (j += 1) {
             var disp = balls.pos[j] - balls.pos[i];
-            var disp_len = abs_v2(disp);
+            var disp_length = abs_v2(disp);
 
-            disp /= @splat(2, disp_len);
+            disp /= @splat(2, disp_length);
 
-            var overlap = balls.radius[i] + balls.radius[j] - disp_len;
+            var overlap = balls.radius[i] + balls.radius[j] - disp_length;
             if (overlap > 0) {
                 overlap /= 2;
 
                 balls.disp[i] -= @splat(2, overlap) * disp;
                 balls.disp[j] += @splat(2, overlap) * disp;
 
-                var perp = if (cross(balls.force[i], disp) > 0)
-                    Vec2{ disp[1], -disp[0] }
-                else
-                    Vec2{ -disp[1], disp[0] };
-
-                balls.force[i] = perp * @splat(2, dot(balls.force[i], perp));
-                if (dot(balls.vel[i], disp) > 0) {
-                    balls.vel[i] = reflect(balls.vel[i], perp);
+                var force_disp_comp = dot(balls.force[i], disp);
+                if (force_disp_comp > 0) {
+                    balls.force[i] -= @splat(2, force_disp_comp) * disp;
                 }
 
-                perp = if (cross(balls.force[j], disp) > 0)
-                    Vec2{ disp[1], -disp[0] }
-                else
-                    Vec2{ -disp[1], disp[0] };
+                if (dot(balls.vel[i], disp) > 0) {
+                    balls.vel[i] = reflect(balls.vel[i], Vec2{ -disp[1], disp[0] });
+                }
 
-                balls.force[j] = perp * @splat(2, dot(balls.force[j], perp));
+                force_disp_comp = dot(balls.force[j], disp);
+                if (force_disp_comp < 0) {
+                    balls.force[j] -= @splat(2, force_disp_comp) * disp;
+                }
+
                 if (dot(balls.vel[j], disp) < 0) {
-                    balls.vel[j] = reflect(balls.vel[j], perp);
+                    balls.vel[j] = reflect(balls.vel[j], Vec2{ -disp[1], disp[0] });
                 }
             }
         }
@@ -316,37 +350,75 @@ fn draw_balls(balls: *Balls) void {
         ray.DrawLineEx(center, norm_to_pixel_v2(balls.pos[i] + balls.vel[i] / @splat(2, @as(f32, 4))), 3, ray.GREEN);
     }
 
-    var spring = SpringInfo{
-        .half_height = 0.05,
-        .hand_length = 0.05,
-        .blade_count = 4,
-    };
-
     i = 0;
-    while (i < balls.count) : (i += 1) {
-        var j: usize = i + 1;
-        while (j < balls.count) : (j += 1) {
-            draw_spring(spring, balls.pos[i], balls.pos[j], ray.GRAY);
-        }
+    for (balls.edges) |edge| {
+        draw_spring(balls, edge, ray.GRAY);
     }
 }
+
+// TODO: make the size of spring hand slighly higher than the radius of ball.
+// TODO: add ability to pause simulation, drag balls, and change their properties
+//       in runtime.
+// TODO: add debugging tools (e.g. toggle physic simulation).
 
 pub fn main() void {
     ray.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello, Traingles!");
     ray.SetTargetFPS(60);
 
     const MAX_BALL_COUNT = 3;
-    var _balls_pos: [MAX_BALL_COUNT]Vec2 = undefined;
+    var _balls_pos: [MAX_BALL_COUNT]Vec2 = .{
+        .{ -0.5, 0.5 },
+        .{ -0.5, -0.5 },
+        .{ 0.5, -0.5 },
+    };
     var _balls_vel: [MAX_BALL_COUNT]Vec2 = undefined;
     var _balls_force: [MAX_BALL_COUNT]Vec2 = undefined;
     var _balls_disp: [MAX_BALL_COUNT]Vec2 = undefined;
-    var _balls_mass = [MAX_BALL_COUNT]f32{ 2, 7, 5 };
+    var _balls_mass = [MAX_BALL_COUNT]f32{ 2, 5, 10 };
     var _balls_color = [MAX_BALL_COUNT]ray.Color{
         ray.MAGENTA,
         ray.BLUE,
-        ray.DARKBLUE,
+        ray.PINK,
     };
-    var _balls_radius = [MAX_BALL_COUNT]f32{ 0.05, 0.05, 0.15 };
+    var _balls_edges = [_]Spring{
+        .{
+            .start = 0,
+            .end = 1,
+            .stiffness = 4.0,
+            .rest_length = 0.15,
+            .min_length = 0.08,
+            .max_length = 1.0,
+            .damping_factor = 1,
+            .half_height = 0.05,
+            .hand_length = 0.05,
+            .blade_count = 3,
+        },
+        .{
+            .start = 0,
+            .end = 2,
+            .stiffness = 4.0,
+            .rest_length = 0.5,
+            .min_length = 0.3,
+            .max_length = 0.8,
+            .damping_factor = 1,
+            .half_height = 0.05,
+            .hand_length = 0.05,
+            .blade_count = 2,
+        },
+        .{
+            .start = 1,
+            .end = 2,
+            .stiffness = 4.0,
+            .rest_length = 0.8,
+            .min_length = 0.3,
+            .max_length = 1.3,
+            .damping_factor = 1,
+            .half_height = 0.05,
+            .hand_length = 0.05,
+            .blade_count = 1,
+        },
+    };
+    var _balls_radius = [MAX_BALL_COUNT]f32{ 0.05, 0.1, 0.2 };
 
     var balls = Balls{
         .pos = &_balls_pos,
@@ -356,6 +428,7 @@ pub fn main() void {
         .mass = &_balls_mass,
         .radius = &_balls_radius,
         .color = &_balls_color,
+        .edges = _balls_edges[0..],
         .count = 3,
     };
 
